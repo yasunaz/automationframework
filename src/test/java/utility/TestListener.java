4a03c5bb1216121c9c8e3b54d29cfe9a0ecc8e26
nijat <nijatmn@gmail.com>
2019-03-16 15:03:20 -0400

    my lastest change

---
+++ src/test/java/utility/TestListener.java
@@ -0,0 +1,38 @@
+package utility;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class TestListener implements ITestListener {
+
+	// =========== TEST ===========================
+	public void onStart(ITestContext arg0) {
+
+	}
+
+	public void onFinish(ITestContext arg0) {
+
+	}
+
+	// ============ TEST CASE =======================
+	public void onTestStart(ITestResult arg0) {
+
+	}
+
+	public void onTestFailedButWithinSuccessPercentage(ITestResult arg0) {
+
+	}
+
+	public void onTestFailure(ITestResult arg0) {
+
+	}
+
+	public void onTestSkipped(ITestResult arg0) {
+
+	}
+
+	public void onTestSuccess(ITestResult arg0) {
+
+	}
+}
